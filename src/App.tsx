83c4459556d0b3472864a0bbf8cf299319a8468d
Pablo-Restrepo <restrepo736@gmail.com>
2025-10-27 21:42:26 -0500

    feat: Implement NotFound page with 404 message and navigation options

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from "@/pages/home/HomePage"
+import NotFound from "@/pages/NotFound"
 import { ThemeProvider } from "@/components/theme-provider"
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </ThemeProvider>
